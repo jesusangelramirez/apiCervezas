4ae57cb9c42165d86cd9996abf1e1c17d9e42e59
jesusangelramirez <jesus.angel.ramirez89@gmail.com>
2020-02-13 16:50:21 +0100

    Creacion de un endpoint para mostrar las cervezas filtradas por comida /cervezas/busquedaPorComida/{comida}

--- src/Controller/CervezaController.php
+++ src/Controller/CervezaController.php
@@ -32,5 +32,32 @@ class CervezaController extends AbstractController
         $response = new JsonResponse($cervezas);
         return $response;
     }
+    /**
+     * @Route("cervezas/busquedaPorComida/{comida}", name="busquedaPorComida")
+     */
+    public function busquedaPorComida(string $comida)
+    {
+        $pagina=0;
+        $count=0;
+        $cervezas=[];
+        do {
+            $httpClient = HttpClient::create();
+            $pagina++;
+            $url='https://api.punkapi.com/v2/beers?food='.$comida.'&page='.$pagina;
+            $response = $httpClient->request('GET', $url);
+            $beers = $response->toArray();
+
+            foreach ($beers as $key=>$beer){
+                $cervezas[$count]['id'] = $beer['id'];
+                $cervezas[$count]['nombre'] = $beer['name'];
+                $cervezas[$count]['descripcion'] = $beer['description'];
+                $count++;
+            }
+
+        }while (!empty($beers));
+        $response = new JsonResponse($cervezas);
+        return $response;
+
+    }
 
 }
